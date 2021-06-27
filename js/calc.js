95dcf4f70da9c216ee7b68162c637cbb5aeaad98
Nikolay <nikolay.eremin82@mail.ru>
2021-06-27 10:57:54 +0600

    Сделал очередной рефакторинг кода

--- js/calc.js
+++ js/calc.js
@@ -1,16 +1,6 @@
 "use strict";
 
-let number;
-let value1;
-let value2;
-let intermediateValue;
 let result;
-let coefficient1;
-let coefficient2;
-let arr;
-let a;
-let b;
-let c;
 let j;
 let num1 = document.querySelectorAll(".num1");
 let num2 = document.querySelectorAll(".num2");
@@ -20,12 +10,7 @@ let resultat = document.querySelectorAll(".resultEnter");
 let btn = document.querySelectorAll(".btn");
 let zakladki = document.querySelector(".zakladki");
 let calculyator = document.querySelectorAll(".calculyator");
-let calcF = document.querySelector(".calcF");
-let calcL = document.querySelector(".calcL");
-let calcC = document.querySelector(".calcC");
-let buttonF = document.querySelector(".buttonF");
-let buttonL = document.querySelector(".buttonL");
-let buttonC = document.querySelector(".buttonC");
+let button = document.querySelectorAll(".button");
 
 let obj = {
     "пГн": 0.000000000001,
@@ -43,30 +28,16 @@ let obj = {
     "кГц": 1000,
     "МГц": 1000000,
     "ГГц": 1000000000,
-    "pH": "пГн",
-    "nH": "нГн",
-    "mkH": "мкГн",
-    "mH": "мГн",
-    "H": "Гн",
-    "kH": "кГн",
-    "mHz": "мГц",
-    "Hz": "Гц",
-    "kHz": "кГц",
-    "MHz": "МГц",
-    "GHz": "ГГц",
-    "pF": "пФ",
-    "nF": "нФ",
-    "mkF": "мкФ",
-    "mF": "мФ",
-    "F": "Ф",
-    "kF": "кФ",
+    "frequency": ["мГц", "Гц", "кГц", "МГц", "ГГц"],
+    "inductance": ["пГн", "нГн", "мГн", "мГн", "Гн", "кГн"],
+    "capacity": ["пФ", "нФ", "мкФ", "мФ", "Ф", "кФ"],
     units: function(key) {
         return obj[key];
     },
     start: function(j) {
-        number = [+num1[j].value, +num2[j].value];
-        coefficient1 = this.units(edinizy1[j].value);
-        coefficient2 = this.units(edinizy2[j].value);
+        let number = [+num1[j].value, +num2[j].value];
+        let coefficient1 = this.units(edinizy1[j].value);
+        let coefficient2 = this.units(edinizy2[j].value);
         return this.numValidator(number, coefficient1, coefficient2, j);
     },
     numValidator: function(number, coefficient1, coefficient2, j) {
@@ -80,6 +51,7 @@ let obj = {
                 result = "Используйте единицы пФ!";
                 return this.resultEnter(result, j);
             }
+            
         } else if (j === 2) {
             if (coefficient1 == 1000000 && coefficient2 > 0.000001) {
                 result = "Используйте единицы мкГн и ниже!";
@@ -93,9 +65,9 @@ let obj = {
         }
     
         if (number[0] !== "" && isNaN(number[0]) === false){
-            value1 = number[0];
+            let value1 = number[0];
             if (number[1] !== "" && isNaN(number[1]) === false) {
-                value2 = number[1];
+                let value2 = number[1];
                 return this.calc(value1, value2, coefficient1, coefficient2, j);
             }
         }
@@ -103,6 +75,7 @@ let obj = {
         this.resultEnter(result, j);
     },
     calc: function(value1, value2, coefficient1, coefficient2, j) {
+        let intermediateValue;
         if (j === 0) {
             intermediateValue = 1 / (2 * Math.PI * Math.sqrt(value1 * coefficient1 * value2 * coefficient2));
         } else if (j === 1) {
@@ -115,26 +88,25 @@ let obj = {
         this.processingTheResult(intermediateValue, j);
     },
     processingTheResult: function(intermediateValue, j) {
+        let a = 0.000000000001;
+        let b = 0.000000001;
+        let c = 100000000000000;
+        let d = 1000000000;
+        let arr;
         if (j === 0) {
-            arr = ["mHz", "Hz", "kHz", "MHz", "GHz"];
-            a = 0.001;
-            b = 1;
-            c = 100000;
+            arr = this.units("frequency");
+            a *= d;
+            b *= d;
+            c /= d;
         } else if (j === 1) {
-            arr = ["pH", "nH", "mkH", "mH", "H", "kH"];
-            a = 0.000000000001;
-            b = 0.000000001;
-            c = 100000000000000;
+            arr = this.units("inductance");
         } else if (j === 2) {
-            arr = ["pF", "nF", "mkF", "mF", "F", "kF"];
-            a = 0.000000000001;
-            b = 0.000000001;
-            c = 100000000000000;
+            arr = this.units("capacity");
         } 
     
         for (let i = 0; i <= 5; i++) {
             if(intermediateValue >= a && intermediateValue < b) {
-                result = `${Math.round(intermediateValue * c) / 100} ${obj.units(arr[i])}`;
+                result = `${Math.round(intermediateValue * c) / 100} ${arr[i]}`;
                 return this.resultEnter(result, j);
             }
             a = a * 1000;
@@ -173,17 +145,9 @@ zakladki.addEventListener("click", (event) => {
     }
 });
 
-buttonF.addEventListener("click", (evt) => {
-    evt.preventDefault();
-    obj.start(0);
-});
-
-buttonL.addEventListener("click", (evt) => {
-    evt.preventDefault();
-    obj.start(1);
-});
-
-buttonC.addEventListener("click", (evt) => {
-    evt.preventDefault();
-    obj.start(2);
+button.forEach((item, i) => {
+    item.addEventListener('click', (evt) => {
+        evt.preventDefault();
+        obj.start(i);
+    })
 });
